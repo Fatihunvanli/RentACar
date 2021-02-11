0580c70e9a51279ec3eed2232adecd1bdb334641
fatihunvanli <fatihunvanl@gmail.com>
2021-02-12 00:42:53 +0300

    GenericRepository added.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -14,9 +14,28 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+            }
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
         }
     }
 }
