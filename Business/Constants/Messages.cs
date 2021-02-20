cf5dfec455b797cd4a46d005768e55ad204ccaa1
fatihunvanli <fatihunvanl@gmail.com>
2021-02-20 19:24:53 +0300

    User, Customer, Rental entityleri eklendi. Tüm crud işlemleri yapıldı.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,16 @@ namespace Business.Constants
         public static string CarNameInvalid = "Araba ismi geçersiz";
         public static string MaintenanceTime="Sistem bakımda";
         public static string CarsListed = "Ürünler listelendi";
+        internal static string CarConnotBeRented;
+        internal static string AddedRental;
+        internal static string DeletedRental;
+        internal static string ReturnedRental;
+        internal static string UpdatedRental;
+        internal static string AddedUser;
+        internal static string DeletedUser;
+        internal static string UpdatedUser;
+        internal static string AddedCustomer;
+        internal static string DeletedCustomer;
+        internal static string UpdatedCustomer;
     }
 }
